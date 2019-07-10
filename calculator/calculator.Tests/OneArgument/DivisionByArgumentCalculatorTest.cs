0eaca72a0f4a70d75c6e198b96d97bc6ae8770f8
SerejkaP <haggardspark@gmail.com>
2019-07-10 23:47:36 +0600

    create test for division of one argument (1/x)

---
+++ calculator/calculator.Tests/OneArgument/DivisionByArgumentCalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.Tests.OneArgument
+{
+    [TestFixture]
+    class DivisionByArgumentCalculatorTest
+    {
+        [TestCase(1, 1)]
+        [TestCase(2, 0.5)]
+        [TestCase(-1, -1)]
+        public void CalculateTest(
+double firstValue,
+double expected)
+        {
+            var calculator = new calculator.OneArgument.DivisionByArgumentCalculator();
+            var actualResult = calculator.Calculate(firstValue);
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
