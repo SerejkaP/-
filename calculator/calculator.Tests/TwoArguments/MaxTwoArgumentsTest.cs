fc5da00c982f588e2c3467080a4de8a7200d54eb
SerejkaP <haggardspark@gmail.com>
2019-07-10 23:17:53 +0600

    Create test for max of two arguments

---
+++ calculator/calculator.Tests/TwoArguments/MaxTwoArgumentsTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.Tests.TwoArguments
+{
+    [TestFixture]
+    class MaxTwoArgumentsTest
+    {
+        [TestCase(0, 0, 0)]
+        [TestCase(3, 4, 4)]
+        [TestCase(-7, -2, -2)]
+        public void CalculateTest(
+double firstValue,
+double secondValue,
+double expected)
+        {
+            var calculator = new calculator.TwoArguments.MaxTwoArguments();
+            var actualResult = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
