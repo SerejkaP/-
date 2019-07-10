d8ceaba5184f9add12a84be427be9a7cab550b2b
kasheev <tactical949@gmail.com>
2019-07-11 01:20:15 +0600

    Create test for log of ten

---
+++ calculator/calculator.Tests/OneArgument/LogOfTenCalculatorTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace calculator.Tests.OneArgument
+{
+    [TestFixture]
+    class LogOgTenCalculatorTest
+    {
+        [TestCase(10, 1)]
+        [TestCase(100, 2)]
+        [TestCase(1, 0)]
+        public void CalculateTest(
+double firstValue,
+double expected)
+        {
+            var calculator = new calculator.OneArgument.LogOfTenCalculator();
+            var actualResult = calculator.Calculate(firstValue);
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
