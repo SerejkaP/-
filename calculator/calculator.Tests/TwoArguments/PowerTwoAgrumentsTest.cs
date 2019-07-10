0adeeffe3def45f0c2fba79416b70ed0abcee8d0
kasheev <tactical949@gmail.com>
2019-07-11 02:13:31 +0600

    create test for power of two arguments

---
+++ calculator/calculator.Tests/TwoArguments/PowerTwoAgrumentsTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+
+namespace calculator.Tests.TwoArguments
+{
+    [TestFixture]
+    class PowerTwoArgumentsTest
+    {
+        [TestCase(2, 2, 4)]
+        [TestCase(3, 5, 243)]
+        [TestCase(5, 2, 25)]
+        public void CalculateTest(
+double firstValue,
+double secondValue,
+double expected)
+        {
+            var calculator = new calculator.TwoArguments.PowerTwoArguments();
+            var actualResult = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
