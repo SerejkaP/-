456082eb498afbde429b6a44ab16480b3b10cc32
kasheev <tactical949@gmail.com>
2019-07-11 01:24:52 +0600

    create test for log of two

---
+++ calculator/calculator.Tests/OneArgument/LogOfTwoCalculatorTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+
+namespace calculator.Tests.OneArgument
+{
+    [TestFixture]
+    class LogOfTwoCalculatorTest
+    {
+        [TestCase(2, 1)]
+        [TestCase(4, 2)]
+        [TestCase(1, 0)]
+        public void CalculateTest(
+double firstValue,
+double expected)
+        {
+            var calculator = new calculator.OneArgument.LogOfTwoCalculator();
+            var actualResult = calculator.Calculate(firstValue);
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
