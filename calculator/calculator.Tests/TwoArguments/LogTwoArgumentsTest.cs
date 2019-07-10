bd6e98c73ce9d52f666d4bfc219b69434a28fc82
kasheev <tactical949@gmail.com>
2019-07-11 02:05:42 +0600

    create test for log ot two arguments

---
+++ calculator/calculator.Tests/TwoArguments/LogTwoArgumentsTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+
+namespace calculator.Tests.TwoArguments
+{
+    [TestFixture]
+    class LogTwoArgumentsTest
+    {
+        [TestCase(1, 2, 0)]
+        [TestCase(10, 100, 0.5)]
+        [TestCase(4, 2, 2)]
+        public void CalculateTest(
+double firstValue,
+double secondValue,
+double expected)
+        {
+            var calculator = new calculator.TwoArguments.LogTwoArguments();
+            var actualResult = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
