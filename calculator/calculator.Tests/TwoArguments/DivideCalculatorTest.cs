f0029ac9f07148c26f3646fe114e57a7000bc981
kasheev <tactical949@gmail.com>
2019-07-11 01:58:40 +0600

    create test for divide of two arguments

---
+++ calculator/calculator.Tests/TwoArguments/DivideCalculatorTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace calculator.Tests.TwoArguments
+{
+    [TestFixture]
+    class DivideCalculatorTest
+    {
+        [TestCase(4, 3, 1)]
+        [TestCase(5, 2, 2)]
+        [TestCase(-7, -3, 2)]
+        public void CalculateTest(
+double firstValue,
+double secondValue,
+double expected)
+        {
+            var calculator = new calculator.TwoArguments.DivideCalculator();
+            var actualResult = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
