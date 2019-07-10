9a3b46015233b3280d4b943d6f7000760b6ac13f
kasheev <tactical949@gmail.com>
2019-07-11 01:34:22 +0600

    created a test for the sine funcion

---
+++ calculator/calculator.Tests/OneArgument/SinusCalculatorTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+
+namespace calculator.Tests.OneArgument
+{
+    [TestFixture]
+    class SinusCalculatorTest
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 0.841)]
+        [TestCase(-1, -0.841)]
+        public void CalculateTest(
+double firstValue,
+double expected)
+        {
+            var calculator = new calculator.OneArgument.SinusCalculator();
+            var actualResult = calculator.Calculate(firstValue);
+            Assert.AreEqual(expected, actualResult, 0.001);
+        }
+    }
+}
