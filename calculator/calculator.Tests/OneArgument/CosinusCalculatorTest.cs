cfdae319fa7b21f0d0d7eabbbf580121c65963a3
kasheev <tactical949@gmail.com>
2019-07-11 01:01:45 +0600

    created a test for the cosine funcion

---
+++ calculator/calculator.Tests/OneArgument/CosinusCalculatorTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+
+namespace calculator.Tests.OneArgument
+{
+    [TestFixture]
+    class CosinusCalculatorTest
+    {
+        [TestCase(0, 1)]
+        [TestCase(1, 0.54)]
+        [TestCase(2, -0.41)]
+        public void CalculateTest(
+double firstValue,
+double expected)
+        {
+            var calculator = new calculator.OneArgument.CosinusCalculator();
+            var actualResult = calculator.Calculate(firstValue);
+            Assert.AreEqual(expected, actualResult, 0.01);
+        }
+    }
+}
