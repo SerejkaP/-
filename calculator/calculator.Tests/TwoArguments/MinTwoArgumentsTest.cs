e7c70575b849f2a67849ed693816fc244885666a
SerejkaP <haggardspark@gmail.com>
2019-07-10 23:09:27 +0600

    Create test for min argument of two arguments

---
+++ calculator/calculator.Tests/TwoArguments/MinTwoArgumentsTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.Tests.TwoArguments
+{
+    [TestFixture]
+    class MinTwoArgumentsTest
+    {
+        [TestCase(0, 0, 0)]
+
+        [TestCase(3, 4, 3)]
+
+        [TestCase(-7, -2, -7)]
+
+        public void CalculateTest(
+
+double firstValue,
+
+double secondValue,
+
+double expected)
+
+        {
+
+            var calculator = new calculator.TwoArguments.MinTwoArguments();
+
+            var actualResult = calculator.Calculate(firstValue, secondValue);
+
+            Assert.AreEqual(expected, actualResult);
+
+        }
+    }
+}
