a4161cdb34346da16a2c76712c75f9d39717e555
kasheev <tactical949@gmail.com>
2019-07-11 01:47:41 +0600

    create test for log one argument

---
+++ calculator/calculator.Tests/OneArgument/LogOneArgumentTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace calculator.Tests.OneArgument
+{
+    [TestFixture]
+    class LogOneArgumentTest
+    {
+        [TestCase(1, 0)]
+        [TestCase(2, 0.693)]
+        [TestCase(3, 1.098)]
+        public void CalculateTest(
+double firstValue,
+double expected)
+        {
+            var calculator = new calculator.OneArgument.LogOneArgument();
+            var actualResult = calculator.Calculate(firstValue);
+            Assert.AreEqual(expected, actualResult,0.001);
+        }
+    }
+}
