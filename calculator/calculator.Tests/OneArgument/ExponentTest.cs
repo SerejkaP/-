b3fa6fccc03784159f4716a0712d34b0f905b6a1
kasheev <tactical949@gmail.com>
2019-07-11 01:10:39 +0600

    created a test for the fuction exponent

---
+++ calculator/calculator.Tests/OneArgument/ExponentTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace calculator.Tests.OneArgument
+{
+    [TestFixture]
+    class ExponentTest
+    {
+        [TestCase(0, 1)]
+        [TestCase(-1, 0.367)]
+        [TestCase(1, 2.718)]
+        public void CalculateTest(
+double firstValue,
+double expected)
+        {
+            var calculator = new calculator.OneArgument.Exponent();
+            var actualResult = calculator.Calculate(firstValue);
+            Assert.AreEqual(expected, actualResult, 0.001);
+        }
+    }
+}
--- calculator/calculator/OneArgument/CosinusCalculator.cs
+++ calculator/calculator/OneArgument/CosinusCalculator.cs
@@ -2,7 +2,7 @@
 
 namespace calculator.OneArgument
 {
-    public class CosinusCalculator :IOneArgumentCalculator
+    public class CosinusCalculator : IOneArgumentCalculator
     {
         public double Calculate(double firstValue)
         {
