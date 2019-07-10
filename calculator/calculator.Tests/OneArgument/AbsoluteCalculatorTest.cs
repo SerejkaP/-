9af10c42414eed0a2515215fe0f9f42a594eacbf
SerejkaP <haggardspark@gmail.com>
2019-07-10 23:42:58 +0600

    Create test for absolute argument

---
+++ calculator/calculator.Tests/OneArgument/AbsoluteCalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.Tests.OneArgument
+{
+    [TestFixture]
+    class AbsoluteCalculatorTest
+    {
+        [TestCase(0,0)]
+        [TestCase(10,10)]
+        [TestCase(-7,7)]
+        public void CalculateTest(
+double firstValue,
+double expected)
+        {
+            var calculator = new calculator.OneArgument.AbsoluteCalculator();
+            var actualResult = calculator.Calculate(firstValue);
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
