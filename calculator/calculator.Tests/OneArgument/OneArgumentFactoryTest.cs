bcc9ba6a502d054f7ddd787ede43b95a3637f831
SerejkaP <haggardspark@gmail.com>
2019-07-11 11:53:48 +0300

    create test for one argument factory

---
+++ calculator/calculator.Tests/OneArgument/OneArgumentFactoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using calculator.OneArgument;
+using NUnit.Framework;
+
+namespace calculator.Tests.OneArgument
+{
+    [TestFixture]
+    class OneArgumentFactoryTest
+    {
+        [TestCase("Power", typeof(PowerCalculator))]
+        [TestCase("SquareRoot", typeof(SquareRootCalculator))]
+        [TestCase("Sinus", typeof(SinusCalculator))]
+        [TestCase("Cosinus", typeof(CosinusCalculator))]
+        [TestCase("LogOfExp", typeof(LogOneArgument))]
+        [TestCase("Exponent", typeof(Exponent))]
+        [TestCase("Tangent", typeof(TangentCalculator))]
+        [TestCase("TwoPowerValues", typeof(TwoPowerValues))]
+        [TestCase("Log10", typeof(LogOfTenCalculator))]
+        [TestCase("Log2", typeof(LogOfTwoCalculator))]
+        [TestCase("DivisionByArgument", typeof(DivisionByArgumentCalculator))]
+        [TestCase("Absolute", typeof(AbsoluteCalculator))]
+        [TestCase("Negative", typeof(NegativeArgumentCalculator))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = OneArgumentFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
