b826b87fdab684db5759276a47e95d2c0872ad24
SerejkaP <haggardspark@gmail.com>
2019-07-11 11:44:57 +0300

    create test for two arguments factory

---
+++ calculator/calculator.Tests/TwoArguments/TwoArgumentsFactoryTest.cs
@@ -0,0 +1,31 @@
+﻿using calculator.TwoArguments;
+using NUnit.Framework;
+using System;
+
+namespace calculator.Tests.TwoArguments
+{
+    [TestFixture]
+    class TwoArgumentsFactoryTest
+    {
+        [TestCase("Division", typeof(DivideCalculator))]
+        [TestCase("Plus", typeof(AdditionCalculator))]
+        [TestCase("Average", typeof(Average))]
+        [TestCase("Divide", typeof(DivisionCalculator))]
+        [TestCase("Geometric", typeof(GeometricMean))]
+        [TestCase("logTwoArguments", typeof(LogTwoArguments))]
+        [TestCase("MAX", typeof(MaxTwoArguments))]
+        [TestCase("MIN", typeof(MinTwoArguments))]
+        [TestCase("Modulo", typeof(ModuloCalculator))]
+        [TestCase("Multiply", typeof(MultiplyCalculator))]
+        [TestCase("PowerOfDivision", typeof(PowerOfDivision))]
+        [TestCase("PowerTwoArguments", typeof(PowerTwoArguments))]
+        [TestCase("Minus", typeof(SubtractionCalculator))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = TwoArgumentsFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+    }
+}
