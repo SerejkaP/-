3b9c71a9b774af7aec147e5f4feb2de0a7aadb65
SerejkaP <haggardspark@gmail.com>
2019-07-11 15:58:45 +0600

    add xml-comment

--- calculator/calculator/TwoArguments/MultiplyCalculator.cs
+++ calculator/calculator/TwoArguments/MultiplyCalculator.cs
@@ -2,6 +2,12 @@
 {
     public class MultiplyCalculator : ICalculator
     {
+        /// <summary>
+        /// this method multiply two arguments
+        /// </summary>
+        /// <param name="firstValue"></param>
+        /// <param name="secondValue"></param>
+        /// <returns></returns>
         public double Calculate(double firstValue, double secondValue)
         {
             return firstValue * secondValue;
